477b2db77d2e29a5360d19e805eaec034999cbb7
Vladimir Urbanovich <v_urbanovich@mail.ru>
2016-03-17 17:46:23 +0300

    Clear worckspace for a new designe

--- css/index.js
+++
@@ -1,7 +0,0 @@
-require('./reset.css');
-require('./day-time.css');
-require('./food.css');
-require('./main-view.css');
-require('./master.css');
-require('./save-menu.css');
-require('./table-view.css');

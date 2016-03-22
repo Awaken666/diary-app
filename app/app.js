2f304e3597f92343ceb85f5ba416832a80901cc1
Vladimir Urbanovich <v_urbanovich@mail.ru>
2016-03-22 23:53:36 +0300

    remove files

--- app/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const index = angular.module('index', []);
-
-index.component('modal', require('./modal-window-component/modal-window-component'));
-index.factory('modal', require('./modal-window-component/service'));
-
-module.exports = index;
